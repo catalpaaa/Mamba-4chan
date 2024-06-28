ccb97345bdd783602020871e11de55b77020df13
Catalpa <caojingzifang@gmail.com>
2024-06-27 22:26:08 -0700

    Fix typo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mamba env create -f environment.yml
 mamba activate mamba-4chan
 mamba env config vars set CUDA_HOME=$CONDA_PREFIX
-mamba activate demansia
+mamba activate mamba-4chan
 git clone https://github.com/Dao-AILab/causal-conv1d
 pip install -e causal-conv1d/
 git clone https://github.com/state-spaces/mamba
